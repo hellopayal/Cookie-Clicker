104775cb8c349382ed2b5af7c2e1e00ce32b09cc
hellopayal <hello.pujara@gmail.com>
2023-02-27 20:19:25 -0800

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import axios from 'axios';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
-function App() {
+import './App.css'
+
+const App = () => {
+
+  const [click, setClick] = useState(0)
+  const [emoji, setEmoji] = useState('🍪')
+
+  const handleClick = () => {
+    setClick(click + 1);
+
+    if (click % 2 === 0 && click >= 2) {
+      setEmoji(emoji + '🍪')
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={handleClick}>Click Me</button>
+      <p>click:{click}</p>
+      <p>{emoji}</p>
+      {click >= 10 && <p>Victory</p>}
     </div>
-  );
+  )
 }
-
 export default App;
